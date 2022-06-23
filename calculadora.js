7f990d50b6eddb664074ec64f88e437ae8e58c5c
Alejandro Maza <ajmm9257@gmail.com>
2022-06-22 21:25:17 -0300

    retoques

---
+++ calculadora.js
@@ -0,0 +1,18 @@
+let operandoA
+let operandoB
+let operacion
+
+//call the elements by means id
+let showinscreen = document.getElementById('pantalla')
+let mas = document.getElementById('mas')
+
+
+
+// use funtions 
+
+function visualise(addnumber) {
+    showinscreen.innerText += addnumber
+   
+}
+
+
